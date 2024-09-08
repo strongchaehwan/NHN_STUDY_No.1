9eb1116216cef4950a415cc8c2af2eea6e9f42c5
lty <lty20020617@gmail.com>
2024-09-08 22:48:10 +0900

    다양하게출력/Novice Low/1분/임태영

---
+++ limtae0/다양하게출력.java
@@ -0,0 +1,8 @@
+public class 다양하게출력 {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year");
+        System.out.println(365);
+        System.out.println("Circumference rate");
+        System.out.println(3.1415926535);
+    }
+}
