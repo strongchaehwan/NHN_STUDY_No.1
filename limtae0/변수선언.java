0dc3e6f5c74b2bae99e7e9e8f917568f7742037a
lty <lty20020617@gmail.com>
2024-09-08 22:49:23 +0900

    변수선언/Novice Low/1분/임태영

---
+++ limtae0/변수선언.java
@@ -0,0 +1,9 @@
+public class 변수선언 {
+    public static void main(String[] args) {
+        int a = 3;
+        String b = "C";
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
